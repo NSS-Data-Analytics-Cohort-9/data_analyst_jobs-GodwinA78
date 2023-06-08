94a21914002ff42b81d67f1ef858a90edf4ed73f
GodwinA78 <garryalan.godwin@gmail.com>
2023-06-07 23:47:31 -0400

    finished through q12b corrected

--- SQL scripts/finished to Q 12b.sql
+++ SQL scripts/finished to Q 12b.sql
@@ -62,10 +62,12 @@ FROM data_analyst_jobs
 WHERE title LIKE '%analyst%';
 
 --How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
-SELECT Count(title) AS title_count
+SELECT COUNT(*) AS title_count
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%analyst%'
-AND title NOT LIKE '%analytics%';
+WHERE title NOT LIKE '%Analyst%' AND title NOT LIKE '%Analytics%';
+
+
+
 
 
 
